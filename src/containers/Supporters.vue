f0febeda494f0375575d301b00da0968df4f242b
Mulia Arifandi Nasution <mul14.net@gmail.com>
2017-11-02 18:46:41 +0700

    Add sponsor and booth

--- src/containers/Supporters.vue
+++ src/containers/Supporters.vue
@@ -1,6 +1,55 @@
 <template>
   <section>
+
+    <div class="bg-light py-1">
+      <div class="container">
+        <div>
+          <div class="mt-5 mb-4">
+            <BigTitle>Our Sponsors</BigTitle>
+          </div>
+
+          <div class="row justify-content-center">
+            <div class="col-8 col-md-2 mb-5" v-for="sponsor in sponsors">
+              <img :src="sponsor.photo" :alt="sponsor.name" class="img-fluid">
+            </div>
+          </div>
+        </div>
+
+        <div>
+          <div class="mt-5 mb-4">
+            <BigTitle>Our Partners</BigTitle>
+          </div>
+
+          <div class="row justify-content-center">
+            <div class="col-8 col-md-2 mb-5" v-for="booth in booths">
+              <img :src="booth.photo" :alt="booth.name" class="img-fluid">
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+
     <div class="container text-light">
+
+      <div class="row">
+
+        <div class="col-12">
+
+          <div class="row justify-content-center mt-5 mb-4">
+            <small class="lead">Community Partners</small>
+          </div>
+
+          <div class="row justify-content-center">
+            <div class="col-8 col-md-2 mb-5" v-for="community in communities">
+              <a :href="community.website">
+                <img :src="community.photo" :alt="community.name" class="img-fluid">
+              </a>
+            </div>
+          </div>
+
+        </div>
+
+      </div>
 
       <div class="row">
 
@@ -48,26 +97,6 @@
 
       </div>
 
-      <div class="row">
-
-        <div class="col-12">
-
-          <div class="row justify-content-center mt-5 mb-4">
-            <small class="lead">Community Partners</small>
-          </div>
-
-          <div class="row justify-content-center">
-            <div class="col-8 col-md-2 mb-5" v-for="community in communities">
-              <a :href="community.website">
-                <img :src="community.photo" :alt="community.name" class="img-fluid">
-              </a>
-            </div>
-          </div>
-
-        </div>
-
-      </div>
-
     </div>
   </section>
 </template>
@@ -80,29 +109,78 @@
 
     data () {
       return {
+        sponsors: [],
+        booths: [],
         communities: [],
       }
     },
 
     async mounted () {
-      try {
-        const res = await
-          AxiosGet('https://api.devsummit.io/api/v1/partners')
-        const communities = res.data.data
+      this.fetchSponsorList()
+      this.fetchBoothList()
+      this.fetchCommunityList()
+    },
 
-        this.communities = communities
-          .filter(community => community.type === 'community')
-          .map(community => {
+    methods: {
+
+      async fetchSponsorList () {
+        try {
+          const res = await AxiosGet('https://api.devsummit.io/api/v1/sponsors')
+          const sponsors = res.data.data
+
+          this.sponsors = sponsors.map(sponsor => {
             return {
-              name: community.name,
-              website: community.website,
-              photo: community.photo.replace(5000, ''),
+              name: sponsor.name,
+              photo: sponsor.attachment,
             }
           })
-      }
-      catch (err) {
-        console.error(err)
-      }
+        }
+        catch (err) {
+          console.error(err)
+        }
+
+      },
+
+
+      async fetchBoothList () {
+        try {
+          const res = await AxiosGet('https://api.devsummit.io/api/v1/booths')
+          const booths = res.data.data
+
+          this.booths = booths.map(booth => {
+            return {
+              name: booth.name,
+              photo: booth.logo_url,
+            }
+          })
+        }
+        catch (err) {
+          console.error(err)
+        }
+
+      },
+
+
+      async fetchCommunityList () {
+        try {
+          const res = await AxiosGet('https://api.devsummit.io/api/v1/partners')
+          const communities = res.data.data
+
+          this.communities = communities
+            .filter(community => community.type === 'community')
+            .map(community => {
+              return {
+                name: community.name,
+                website: community.website,
+                photo: community.photo,
+              }
+            })
+        }
+        catch (err) {
+          console.error(err)
+        }
+
+      },
     },
   }
 </script>
